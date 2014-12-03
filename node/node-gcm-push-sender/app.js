48e346c42b07caeba6c49c4405b83396688bb4ff
Kavimal Wijewardana <kavi707@gmail.com>
2014-12-03 12:30:00 +0530

    Update sample application for Node example for push notification

--- node/node-gcm-push-sender/app.js
+++ node/node-gcm-push-sender/app.js
@@ -16,6 +16,7 @@ var message = new gcm.Message({
 
 var sender = new gcm.Sender('AIzaSyCAhLXMzr_Xzw_6unZ3zoMc-QsrKCkudas');
 //var registrationIds = ['APA91bGOxxE7nHKp76MAqOjSQqwz_C-yvJtVw-_X-AbVr7DJLaCBQz0Ylps5jkow10XSrhbfD_tVtM7C_zRa-Vm_YRuaaNN1-fQGQbndaAQIZ9At6In3roSBXQwKJcV_19OANY9jGeBGTTfHN45V4zC9xXTIAb-goQ'];
+var registrationIds = ['APA91bGfSNGwYvpKbikoPhvy9jC8W3tXXCF97dw15c-D1WgrrskkmwXTDKnGKFAoZP_9MVUHfWulNH1d5XK-ZxChbQCK9cuKUzDrPzgflZQXBpWmmO3MW8sovdEOxBX7b1R8bDPaiCruMixO2f1FcNn8lFoZNyHp6Hll1E_LhX6BDA_gDfkHJjc'];
 var registrationIds = ['APA91bER0aPdFg5__y6YOfjrSw49tdd4UsJUQl47aX1_3hJuFmbortDSfvQbNaYaxoLfCUkRQq3-5_-_hyXadL6j3ISsBdUFsYZpiJ1R9nAt7iGlHmXMOi84NDbdz0uKfWe4Sfv_W61jm93usWWL2TbXy4WPTLESmnId-KPGIY71S9zZ35OSVqs'];
 
 // OPTIONAL
--- node/node-http-server/app.js
+++ node/node-http-server/app.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
  , io = require('socket.io').listen(app)
  , fs = require('fs')
 
-app.listen(3000);
+app.listen(3001);
 
 function handler (req, res) {
  fs.readFile(__dirname + '/index.html',
